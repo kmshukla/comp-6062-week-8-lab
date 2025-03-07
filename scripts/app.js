7a2f644bb2c5facc4c09f76949c3da880d4e22cb
kmshukla <kangkanm.work@gmail.com>
2025-03-06 20:03:59 -0500

    Week 8 Lab

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+console.log('JS loaded!');
+const app = Vue.createApp({
+    data(){
+        return{
+            mainHeading: "Welcome to Vue.js!",
+            message: "This message is displayed based on a boolean property.",
+            showMessage: true,
+        }
+    },
+
+});
+app.mount('#app');
